d88526a0b6ac50a7796037f363f7246692d88dc9
warishahanif <156709950+warishahanif@users.noreply.github.com>
2024-01-17 16:27:46 +0500

    Add files via upload

---
+++ warisha.py
@@ -0,0 +1,33 @@
+a= input("enter the value of a:")
+operator=input("enter the operator (+,-,*,/)")
+b=input("enter the value of b:")
+
+a=int(a)
+b=int(b)          
+if operator == "+":
+   print(a + b)
+s= input("enter the value of s:")
+operator=input("enter the operator (+,-,*,/)")
+t=input("enter the value of t:")
+ 
+s=int(s)
+t=int(t)
+if operator =="-":
+    print(s - t)
+    s= input("enter the value of s:")
+operator=input("enter the operator (+,-,*,/)")
+t=input("enter the value of t:")
+ 
+s=int(s)
+t=int(t)
+if operator =="*":
+    print(s * t)
+    
+a= input("enter the value of a:")
+operator=input("enter the operator (+,-,*,/)")
+b=input("enter the value of b:")
+
+a=int(a)
+b=int(b)          
+if operator == "/":
+   print(a / b)
